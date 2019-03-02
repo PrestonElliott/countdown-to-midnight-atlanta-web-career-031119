fa351819ac970bc13a7cc23cd6d20a9b29d05a8c
Preston Elliott <preston.elliott24@gmail.com>
2019-03-02 16:08:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,9 +4,19 @@ def countdown(x)
   x = 11 
   while x > 1
     x -= 1 
+    puts "#{x} SECOND(S) !"
+  end
+  x = 0 
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(x)
+  x = 11 
+  while x > 1
+    x -= 1 
     sleep(1)
     puts "#{x} SECOND(S) !"
   end
   x = 0 
-  puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
 end
